33e579e2302d87e5ec6a00f4a611da623a4d70e8
Heloísa Fleury <149387177+helofleury@users.noreply.github.com>
2024-04-25 19:13:21 -0300

    Add files via upload

---
+++ Etec.Empresa.BO/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+
+
+
+namespace Etec.Empresa.BO
+{
+    public class Cliente
+    {
+       public string Nome  { get; set; }
+       public string CPF { get; set; }
+       public DateTime DtNascimento{ get; set; }
+       public string Rua { get; set; }
+       public int Numero { get; set; }
+
+        public void cadastrarCliente(Cliente cliente)
+        {
+            string conexaoBD = "server=localhost;database=aula;uid=root;pwd=etec";
+
+            MySqlConnection minhaconexao = new MySqlConnection(conexaoBD);
+            minhaconexao.Open();
+            string comandoSql = "insert into cliente (CPF, Nome, dtNascimento, rua, numero) values('" + cliente.CPF + " ','" + cliente.Nome + "','" + cliente.DtNascimento.ToString("yyyy-MM-dd") + "','" + cliente.Rua + "','" + cliente.Numero + "')";
+            MySqlCommand comando = new MySqlCommand(comandoSql, minhaconexao);
+
+            comando.ExecuteNonQuery();
+        }
+    }
+}
